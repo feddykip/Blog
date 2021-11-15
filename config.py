dff9077d32b3710c07d3171ddf7563221ffa41a8
feddykip <ferdinand.kiplangat@student.moringaschool.com>
2021-11-15 23:27:41 +0300

    modified template

--- config.py
+++ config.py
@@ -20,10 +20,8 @@ class Config:
 
 
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
-    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-            "postgres://", "postgresql://", 1)
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
 
 
 class TestConfig(Config):
